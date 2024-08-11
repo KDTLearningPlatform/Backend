9e519e4c9fb50beaf6c02b6172c02e2f18261a17
retina3084 <106227154+retina3084@users.noreply.github.com>
2024-08-12 01:34:52 +0900

    -- 좋아요 여부 로직 추가

---
+++ src/main/java/ac/su/learningplatform/repository/LoveRepository.java
@@ -0,0 +1,12 @@
+package ac.su.learningplatform.repository;
+
+import ac.su.learningplatform.domain.Love;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoveRepository extends JpaRepository<Love, Long> {
+
+    Love findByUser_UserIdAndStudy_StudyId(Long userId, Long studyId);
+
+}
