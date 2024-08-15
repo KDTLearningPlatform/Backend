a1afb7d5a3c36bb9ab1754585658d54146d79390
n3ptune <n3ptune28@gmail.com>
2024-08-16 07:15:07 +0900

    feat: 내 강의 상세 보기 get (비디오 진행도 포함)

---
+++ src/main/java/ac/su/learningplatform/dto/MyLectureDetailsDTO.java
@@ -0,0 +1,28 @@
+package ac.su.learningplatform.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MyLectureDetailsDTO {
+    private String lectureTitle;
+    private List<VideoProgressDTO> videos;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class VideoProgressDTO {
+        private Long videoId;
+        private String title;
+        private int runningTime;
+        private float progress;
+    }
+}
